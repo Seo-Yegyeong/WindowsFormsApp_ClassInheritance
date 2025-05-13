782abb7e242b2e35edea4bd0fe52af330eaeb1e7
Seo-Yegyeong <seoye6618@gmail.com>
2025-05-13 11:15:15 +0900

    feat: Practice the Class Inheritance and Upcasting&Downcasting

---
+++ WindowsFormsApp_ClassInheritance/Class3Animal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_ClassInheritance
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public Animal() { }
+
+        public void Speak()
+        {
+            Console.WriteLine($"Animal Class :~)");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public void Cry()
+        {
+            Console.WriteLine($"Cat : nyaong :~)");
+        }
+    }
+
+    public class Bird : Animal
+    {
+        public void Sing()
+        {
+            Console.WriteLine($"Bird : JJeakk :~)");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public void Shout()
+        {
+            Console.WriteLine($"Dog : wal wal :~)");
+        }
+    }
+}
---
+++ WindowsFormsApp_ClassInheritance/Form3Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_ClassInheritance
+{
+    public partial class Form3Animal : Form
+    {
+        public Form3Animal()
+        {
+            InitializeComponent();
+
+            //Case #1. Dog
+            Animal animal = new Dog();
+            animal.Speak();
+            // animal.Shout(); //Dog()의 method 사용 불가. 아래와 같이 다운캐스팅 해줘야 함.
+            ((Dog)animal).Shout();
+
+            //Case #2. Cat
+            animal = new Cat();
+            animal.Speak();
+            // animal.Cry(); //Cat()의 method 사용 불가. 아래와 같이 다운캐스팅 해줘야 함.
+            ((Cat)animal).Cry();
+
+            //Case #3. Bird
+            animal = new Bird();
+            animal.Speak();
+            // animal.Sing(); //Bird()의 method 사용 불가. 아래와 같이 다운캐스팅 해줘야 함.
+            ((Bird)animal).Sing();
+
+
+        }
+    }
+}
