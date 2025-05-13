07962ec88d585d52ef00e9f9d8a5184c969db0d7
Seo-Yegyeong <seoye6618@gmail.com>
2025-05-13 11:20:09 +0900

    feat: Practice the Class Inheritance

---
+++ WindowsFormsApp_ClassInheritance/Form2Weapon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_ClassInheritance
+{
+    public partial class Form2Weapon : Form
+    {
+        public Form2Weapon()
+        {
+            InitializeComponent();
+            Sword sword = new Sword();
+            sword.Name = "롱소드";
+            sword.Attact(); //call the parent's method
+            sword.Sharp(); //call the method in child
+
+            Console.WriteLine("==========================");
+
+            /*
+             #2. 업캐스팅: "자식(Sword)" 클래스를 "부모(Weapon)" 클래스 타입으로. 자식 클래스의 고유 기능은 숨겨지고, 부모 클래스의 메서드만 사용할 수 있다.)
+             왜 써?
+             => 다형성 통해 여러 자식 객체를 부모 타임 하나로 통합하여
+                유연하고 재사용 가능한 코드로 작성하기 위해.
+             방식 3가지?
+             1. 묵시적 업캐스팅 (중간에 변수를 할용해서 사용함)
+                Sword sword = new Sword();
+                Weapon weapon1 = sword; //Call the parent's method
+                weapon1.Attact();
+                //weapon1.Sharp(); // Nope. Error 발생. 부모 클래스로 덮어지기 때문에 자식의 클래스 사용 X
+                Weapon 
+             2. 다이렉트(=묵시적) 업캐스팅 (바로 대입하는 방식, 가장 많이 쓰임)
+                Weapon weapon2 = new Gun();
+                weapon2.Name = "Ak-47";
+                weapon2.Attact()
+                //weapon2.Reload(); // 자식 고유 메서드 호출. 에러 발생
+             3. 명시적 업캐스팅 (형변환 기호 사용, 잘 안 씀)
+                Weapon weapon3 = (Weapon)new Bow();
+                weapon.Name = "";
+            */
+        }
+    }
+}
---
+++ WindowsFormsApp_ClassInheritance/Weapon.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_ClassInheritance
+{
+    #region Parent Class
+    public class Weapon
+    {
+        protected int damage; //자기 클래스와 자식 클래스만 이용할 수 있는 값
+        public string Name { get; set; }
+
+
+        public Weapon() { }
+        public Weapon(int damage)
+        {
+            this.damage = damage;
+        }
+        public void Attact()
+        {
+            Console.WriteLine($"{Name}(으)로 공격!");
+        }
+
+        // [Add] Damage Return Method (Arrow Function)
+        public int AttactPower() => damage;
+    }
+    #endregion
+
+
+    #region Child Class
+    public class Sword : Weapon //검
+    {
+        //protected int attact_range;
+        public Sword() { }
+        public Sword(int damage) : base(damage) { }
+
+        public void Sharp()
+        {
+            Console.WriteLine("검이 날카롭게 빛납니다.");
+        }
+    }
+
+    public class Gun : Weapon //총
+    {
+        // [Add] Attact Abailable range
+        int attact_range = 1;
+
+            public void Reload()
+        {
+            Console.WriteLine("총을 재장전합니다.");
+        }
+
+        // [Add]
+        public int Slash(int range)
+        {
+            if (this.attact_range >= range)
+            {
+                return this.damage;
+            }
+            return 0;
+        }
+    }
+
+    public class Bow : Weapon //활
+    {
+        public void Aim()
+        {
+            Console.WriteLine("활을 당겨 조준합니다.");
+        }
+    }
+    #endregion
+}
